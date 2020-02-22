70758bef36c9b88b3e6857af4a4365ea94c8ba29
Oleksandr Tryshchenko <o.tryshchenko@relay42.com>
2020-02-22 14:44:29 +0100

    Make it great again

--- game.js
+++ game.js
@@ -1,71 +1,96 @@
-(() => {
-    const MESSAGE_DELAY = 3000;
-    // The only side effect for each function
-    const state = {
-        dutchWord: 'init',
-        options: ['one', 'two', 'three', 'four'],
-        answer: 'three',
-        guessed: 0,
-        iterate: () => {}
-    };
+const Authentication = new Promise((res, rej) => {
+  const user = netlifyIdentity.currentUser();
+  console.log({ user });
+  if (!user) {
+    netlifyIdentity.open();
+    netlifyIdentity.on("init", user => res(user));
+    netlifyIdentity.on("login", user => res(user));
+  }
+  netlifyIdentity.on("close", () => rej(user));
+})(() => {
+  const MESSAGE_DELAY = 3000;
+  // The only side effect for each function
+  const state = {
+    dutchWord: "init",
+    options: ["one", "two", "three", "four"],
+    answer: "three",
+    guessed: 0,
+    iterate: () => {}
+  };
 
-    let DOM = {}
+  let DOM = {};
 
-    const randomizer = () => Math.random() > 0.5 ? 1 : -1;
+  const randomizer = () => (Math.random() > 0.5 ? 1 : -1);
 
-    const notify = (message) => {
-        if (!message.length && this.timer) {
-            clearTimeout(this.timer);
-            this.timer = null;
-        }
-        DOM.result.innerHTML = message;
-        DOM.guessed.innerHTML = state.guessed;
-        if (message.length) {
-            this.timer = setTimeout(_ => notify(''), MESSAGE_DELAY);
-        }
-        !message.length && state.iterate();
+  const notify = message => {
+    if (!message.length && this.timer) {
+      clearTimeout(this.timer);
+      this.timer = null;
     }
-
-    const handleClick = (id) => (event) => {
-        if (this.timer) return;
-        if (event.target.innerHTML === state.answer) {
-            state.guessed++;
-            return notify('Correct');
-        }
-        state.guessed = 0;
-        return notify(`Wrong. Correct was "${state.answer}"`);
+    DOM.result.innerHTML = message;
+    DOM.guessed.innerHTML = state.guessed;
+    if (message.length) {
+      this.timer = setTimeout(_ => notify(""), MESSAGE_DELAY);
     }
+    !message.length && state.iterate();
+  };
 
-    const next = ({options, listeners, words}) => () => {
-        const list = [...words].sort(randomizer).slice(0, 4);
-        state.dutchWord = list[0][1];
-        state.answer = list[0][2];
-        [...options].sort(randomizer).map((option, i) => {
-            option.innerHTML = list[i][2];
-        });
-        DOM.dutch.innerHTML = state.dutchWord;
+  const handleClick = id => event => {
+    if (this.timer) return;
+    event.target.classList.add("selected");
+    if (event.target.innerHTML === state.answer) {
+      state.guessed++;
+      document.body.classList.add("success");
+      return notify("Correct");
     }
+    document.body.classList.add("failure");
+    state.guessed = 0;
+    return notify(`Wrong. Correct was "${state.answer}"`);
+  };
 
-    window.onload = function() {
-        const ref = document.getElementById.bind(document);
-        const options = [
-            ref('option-1'),
-            ref('option-2'),
-            ref('option-3'),
-            ref('option-4')
-        ];
+  const next = ({ options, listeners, words }) => () => {
+    const list = [...words].sort(randomizer).slice(0, 4);
+    state.dutchWord = list[0][1];
+    state.answer = list[0][2];
+    [...options].sort(randomizer).map((option, i) => {
+      option.innerHTML = list[i][2];
+      option.classList.remove("selected");
+      document.body.classList.remove("success", "failure");
+    });
+    DOM.dutch.innerHTML = state.dutchWord;
+    document.activeElement.blur();
+  };
 
-        DOM = Object.assign({
-            result: ref('result'),
-            guessed: ref('guessed'),
-            dutch: ref('dutch')
-        }, DOM);
+  window.onload = function() {
+    const ref = document.getElementById.bind(document);
 
-        const listeners = options.map((option, i) => 
-            option.addEventListener('click', handleClick(i))    
-        );
+    Authentication.then(user => {
+      const options = [
+        ref("option-1"),
+        ref("option-2"),
+        ref("option-3"),
+        ref("option-4")
+      ];
 
-        state.iterate = next({options, listeners, words: window.$wordsApp.vocabulary});
-        state.iterate();
-    }
+      DOM = Object.assign(
+        {
+          result: ref("result"),
+          guessed: ref("guessed"),
+          dutch: ref("dutch")
+        },
+        DOM
+      );
+
+      const listeners = options.map((option, i) =>
+        option.addEventListener("click", handleClick(i))
+      );
+
+      state.iterate = next({
+        options,
+        listeners,
+        words: window.$wordsApp.vocabulary
+      });
+      state.iterate();
+    }).catch(error => console.error(error));
+  };
 })();
